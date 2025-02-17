037d7df11e8a0462716f0f96bd07be9ebdd37ce4
Skylabs <code@skylabstech.com>
2025-02-17 18:02:00 +0530

    register api done

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors"
 import  "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 
 //app config
 
@@ -17,6 +19,7 @@ app.use(cors());
 //api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
 
 app.get("/", (req,res)=> {
     res.send('API Working')
